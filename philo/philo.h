3fdaf229bcda672aff66183b0c9b826a8afdb46b
sakemyali <emoulayali54@gmail.com>
2026-05-12 23:05:57 +0900

    add: create main.c, philo.h, and utils.c files with initial structure

---
+++ philo/philo.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mosakura <mosakura@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 17:25:24 by mosakura          #+#    #+#             */
+/*   Updated: 2026/05/12 17:46:54 by mosakura         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef PHILO_H
+# define PHILO_H
+
+# include <stdio.h> //printf
+# include <unistd.h> // usleep, fork
+# include <stdlib.h> //malloc, free
+# include <pthread.h> //mutex, threads
+# include <sys/time.h> //gettimeofday
+# include <stdbool.h>
+# include <limits.h> //INTMAX
+
+
+
+#endif
