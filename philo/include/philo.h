49a9458b990867d8e524139625e19d4e1165a2a9
sakemyali <emoulayali54@gmail.com>
2026-05-15 17:38:27 +0900

    add: create philo.h, allocation.c, error.c, logs.c, main.c, parsing.c, and utils.c with initial implementations and structure

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: mosakura <mosakura@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/12 17:25:24 by mosakura          #+#    #+#             */
-/*   Updated: 2026/05/14 12:52:03 by mosakura         ###   ########.fr       */
+/*   Updated: 2026/05/15 16:43:30 by mosakura         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,36 @@
 # include <stdbool.h>
 # include <limits.h> //INTMAX
 
+typedef struct s_table	t_table;
 
+typedef struct s_fork
+{
+	pthread_mutex_t	fork;
+	int				fork_id;
+}				t_fork;
 
+typedef struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		full;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+	t_table		*table;
+}				t_philo;
+
+struct s_table
+{
+	long	philo_nbr;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	nbr_limit_meals;
+	long	start_simulation;
+	bool	end_simulatiosn;
+	t_fork	*forks;
+	t_philo	*philos;
+};
 #endif
